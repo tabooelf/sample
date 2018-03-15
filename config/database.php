c504028c475b03626e73e823973bd586a9accceb
tabooelf <tabooelf@qq.com>
2018-03-15 08:57:36 +0000

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$db_config = get_db_config();
 return [
 
     /*
@@ -12,8 +12,8 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
+    // 'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            'database' => $db_config['database'],
+            // 'username' => env('DB_USERNAME', 'forge'),
+            'username' => $db_config['username'],
+            // 'password' => env('DB_PASSWORD', ''),
+            'password' => $db_config['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
