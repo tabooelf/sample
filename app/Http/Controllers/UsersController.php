d31f06d3ee340ea2eccad69a1fcc986150010173
tabooelf <tabooelf@qq.com>
2018-03-15 08:16:58 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,12 +1,33 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace app\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     public function create(){
         return view('users.create');
+    }
+
+    public function show(User $user){
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'email'    => 'required|email|unique:users|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        session()->flash('success','新用户注册成功,开始新的旅程吧');
+        return redirect()->route('users.show', [$user]);
     }
 }
---
+++ app/Validate/BaseValidate.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace app\Validate;
+
+class BaseValidate{
+
+}
