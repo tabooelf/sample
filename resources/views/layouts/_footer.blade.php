278cc072988d6d4ee611e0b93ebabdb8101a0abc
tabooelf <tabooelf@qq.com>
2018-03-19 10:07:23 +0000

    modify readme && blade fix

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,8 @@
 <div class="col-md-12">
   <footer class="footer">
     <small class="slogon">
-      <img class="brand-icon" src="https://lccdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-      <a href="https://laravel-china.org/courses">
-        刻意练习，每日精进
+      <a href="#">
+        天道酬勤
       </a>
     </small>
     <nav>
